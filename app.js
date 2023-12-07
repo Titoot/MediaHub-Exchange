2ed0f5c19c891fe1eb7476226e9c092285212d20
Titoot <82727556+Titoot@users.noreply.github.com>
2023-12-07 21:48:50 +0200

    add localSteamIds.json checker

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 const app = require("./index");
 const utils = require("./utils");
 const server = http.createServer(app);
+const fs = require('fs');
 
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
@@ -9,7 +10,9 @@ const port = process.env.PORT || API_PORT;
 
 const startServer = async () => {
 
-  //await utils.updateSteamIds();
+  if (!fs.existsSync("./localSteamIds.json")) {
+    await utils.updateSteamIds();
+  }
 
   const fetchInterval = 24 * 60 * 60 * 1000; // 24 hours
 
