4dabf4b47bc55d36b55470d26d5d4ec0c339135b
Titoot <82727556+Titoot@users.noreply.github.com>
2023-12-08 10:08:01 +0200

    apply files types to folders and subfolders schema

--- model/folders.js
+++ model/folders.js
@@ -1,14 +1,62 @@
 const mongoose = require('mongoose');
 
 const fileSchema = new mongoose.Schema({
-  name: String,
-  content: String
+  name: {type: String, unique: true},
+  content: String,
+  size: { type: String, default: '-' },
+  typeModel: {
+    type: String,
+    enum: ['Game', 'Movie', 'Series', 'Anime'],
+  },
+  contentDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: 'typeModel',
+  },
+  modifiedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+const subfolderSchema = new mongoose.Schema({
+  name: {type: String, unique: true},
+  files: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'File', // Reference to the File model
+  }],
+  subfolders: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subfolder',
+    default: null
+  }],
+  modifiedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Subfolder = mongoose.model('Subfolder', subfolderSchema);
 
 const folderSchema = new mongoose.Schema({
-  name: String,
-  files: [fileSchema],
-  subfolders: { type: mongoose.Schema.Types.Mixed, default: [] }
+  name: {type: String, unique: true},
+  //Owner: { type: mongoose.Schema.Types.ObjectId, default: null },
+  files: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'File',
+    default: null
+  }],
+  subfolders: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subfolder',
+    default: null
+  }],
+  modifiedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Folder', folderSchema);
+const File = mongoose.model('File', fileSchema);
+const Folder = mongoose.model('Folder', folderSchema);
+
+module.exports = { File, Folder, Subfolder };
