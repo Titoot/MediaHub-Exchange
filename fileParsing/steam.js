beb26a5d9fac8cc4fc759580745339363729e0ff
Titoot <82727556+Titoot@users.noreply.github.com>
2023-12-11 17:12:21 +0200

    change steam get id parser

--- fileParsing/steam.js
+++ fileParsing/steam.js
@@ -1,40 +1,15 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 var fsp = require('fs/promises');
 
 exports.getSteamId = async (name) =>
 {
-  const localSteamIds = await fsp.readFile("./localSteamIds.json")
-  const SteamIds = JSON.parse(localSteamIds.toString()).applist.apps
-  const dataLength = Object.keys(SteamIds).length
-  for(var i = 0; i < dataLength; i++) {
-    var id = SteamIds[i].name.toLowerCase().replace(/[^\x20-\x7E]/g, '').replace(':', '')
-    const forbidArray = ['dlc', 'add-on', 'season pass', 'pack', 'Soundtrack', 'demo']
-    const checker = forbidArray.some(v => id.includes(v))
+    const url = `https://store.steampowered.com/search/?term=${name}`
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-    if(checker)
-    {
-      continue
-    }
+    const appidValue = $('#search_resultsRows > a:nth-child(1)').attr('data-ds-appid');
 
-    if (id.includes(name.toLowerCase())){
-        return SteamIds[i].appid;
-    }
-}
-return -1;
-}
-
-exports.updateSteamIds = async () => {
-    const steamIdsUrl = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/';
-    const localFilePath = './localSteamIds.json';
-    try {
-        const response = await axios.get(steamIdsUrl);
-        const newData = response.data;
-    
-        // Update local file
-        const jsonString = JSON.stringify(newData)
-        await fsp.writeFile(localFilePath, jsonString);
-        console.log('Local file updated successfully.');
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
+    return appidValue
 }
