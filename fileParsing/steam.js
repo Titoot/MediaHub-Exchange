8ddd9fd6c00e36cca3f7005f07a530f4c7450b6e
Titoot <82727556+Titoot@users.noreply.github.com>
2023-12-13 14:02:19 +0200

    disable json parsing for axios, and invalid check

--- fileParsing/steam.js
+++ fileParsing/steam.js
@@ -5,11 +5,18 @@ var fsp = require('fs/promises');
 exports.getSteamId = async (name) =>
 {
     const url = `https://store.steampowered.com/search/?term=${name}`
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+        transformResponse: x => x
+    }); // to disable json parsing
     const html = response.data;
     const $ = cheerio.load(html);
 
     const appidValue = $('#search_resultsRows > a:nth-child(1)').attr('data-ds-appid');
+
+    if(!appidValue)
+    {
+        return -1;
+    }
 
     return appidValue
 }
