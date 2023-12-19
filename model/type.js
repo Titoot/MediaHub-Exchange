a4a31556b266ee7269f2cc613f19ca8f08eb1dd4
Titoot <82727556+Titoot@users.noreply.github.com>
2023-12-19 09:34:31 +0200

    edit db schema to add new entries

--- model/type.js
+++ model/type.js
@@ -1,30 +1,41 @@
 const mongoose = require('mongoose');
 
 const gameSchema = new mongoose.Schema({
-  steamid: { type: String, default: null },
-  genre: { type: String, default: null },
+  mediaId: { type: String, default: null },
+  genre: { type: [String], default: [] },
   headerImage: { type: String, default: null },
   description: { type: String, default: null },
+  release: { type: String, default: null },
 });
 
 const movieSchema = new mongoose.Schema({
-  genre: { type: String, default: null },
+  mediaId: { type: String, default: null },
+  genre: { type: [String], default: [] },
   headerImage: { type: String, default: null },
   description: { type: String, default: null },
+  release: { type: String, default: null },
 });
 
 const seriesSchema = new mongoose.Schema({
+  mediaId: { type: String, default: null },
   seasons: { type: String, default: null },
-  genre: { type: String, default: null },
+  episodes: { type: String, default: null },
+  genre: { type: [String], default: [] },
   headerImage: { type: String, default: null },
   description: { type: String, default: null },
+  release: { type: String, default: null },
+  status: { type: String, default: null },
 });
 
 const animeSchema = new mongoose.Schema({
+  mediaId: { type: String, default: null },
   seasons: { type: String, default: null },
-  genre: { type: String, default: null },
+  episodes: { type: String, default: null },
+  genre: { type: [String], default: [] },
   headerImage: { type: String, default: null },
   description: { type: String, default: null },
+  release: { type: String, default: null },
+  status: { type: String, default: null },
 });
 
 const Game = mongoose.model('Game', gameSchema);
@@ -35,4 +46,6 @@ const Series = mongoose.model('Series', seriesSchema);
 
 const Anime = mongoose.model('Anime', animeSchema);
 
-module.exports = { Game, Movie, Series, Anime };
+module.exports = {
+  Game, Movie, Series, Anime,
+};
